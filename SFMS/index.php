65f1573998e21f8e0b7fd9d4c2e8c0373fe3fae0
ABHIJIT WANKHEDE <30998039+abhi27w@users.noreply.github.com>
2021-02-23 00:17:05 +0530

    Add files via upload

---
+++ SFMS/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>TCS inframind File Management System</title>
+		<meta charset = "utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel = "stylesheet" type="text/css" href="admin/css/bootstrap.css" />
+		<link rel = "stylesheet" type="text/css" href="admin/css/style.css" />
+	</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top" style="background-color:blue;">
+		<div class="container-fluid">
+			<label class="navbar-brand" id="title">TCS inframind File Management System</label>
+		</div>
+	</nav>
+	<?php include 'login.php'?>
+	
+</body>
+</html>
